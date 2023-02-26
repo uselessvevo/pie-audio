725478e6f703bb37c62564d2fdf6bdc1b660bc2a
uselessvevo <jcommander13@yandex.ru>
2023-02-26 16:42:59 +0500

    Small changes in "About" plugin

--- src/pieapp/plugins/about-app/plugin/plugin.py
+++ src/pieapp/plugins/about-app/plugin/plugin.py
@@ -1,13 +1,14 @@
 import typing
 
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QWidget, QLabel
+from PyQt5.QtWidgets import QLabel, QWidget, QGridLayout, QPushButton, QDialog
 
-from pieapp.types import Containers
+from pieapp.structs import Containers, Plugins
 from piekit.plugins.base import PiePlugin
 from piekit.plugins.mixins import MenuAccessor
 
-from piekit.managers.types import Sections
+from piekit.system.loader import Config
 from piekit.managers.assets.mixins import AssetsAccessor
 from piekit.managers.locales.mixins import LocalesAccessor
 from piekit.managers.plugins.decorators import onPluginAvailable
@@ -19,25 +20,45 @@ class About(
     LocalesAccessor,
     AssetsAccessor,
 ):
-    name = "about-app"
-    requires = ["menu-bar"]
+    name = Plugins.About
+    requires = [Containers.MenuBar]
 
     def init(self) -> None:
-        self.widget = QWidget()
-        self.widget.setWindowTitle(self.getTranslation("About app"))
+        self.widget = QDialog(self.parent())
+        self.widget.setWindowTitle(self.getTranslation("About"))
+
+        okButton = QPushButton(self.getTranslation("Ok"))
+        okButton.clicked.connect(self.widget.close)
 
         pixmap = QPixmap()
         pixmap.load(self.getAsset("cloud.png"))
 
-        appIcon = QLabel(self.widget)
-        appIcon.setPixmap(pixmap)
+        iconLabel = QLabel()
+        iconLabel.setPixmap(pixmap)
 
-        self.widget.setWindowIcon(self.getAssetIcon("plugin.png"))
+        descriptionLabel = QLabel()
+        descriptionLabel.setText("Pie Audio • Audio Converter ({})".format(
+            Config.PIEAPP_VERSION
+        ))
+
+        githubLinkLabel = QLabel()
+        githubLinkLabel.setOpenExternalLinks(True)
+        githubLinkLabel.setText("<a href='https://github.com/uselessvevo/pie-audio/'>Project URL</a>")
+
+        gridLayout = QGridLayout()
+        gridLayout.addWidget(iconLabel, 0, 0, alignment=Qt.AlignHCenter)
+        gridLayout.addWidget(descriptionLabel, 1, 0, alignment=Qt.AlignHCenter)
+        gridLayout.addWidget(githubLinkLabel, 2, 0, alignment=Qt.AlignHCenter)
+        gridLayout.addWidget(okButton, 3, 0, alignment=Qt.AlignRight)
+
+        self.widget.setLayout(gridLayout)
+        self.widget.setWindowIcon(self.getAssetIcon("help.png"))
         self.widget.resize(400, 300)
 
     @onPluginAvailable(target=Containers.MenuBar)
     def onMenuBarAvailable(self) -> None:
-        self.getMenu(Sections.Shared, "help").addMenuItem(
+        self.addMenuItem(
+            menu="help",
             name="about",
             text=self.getTranslation("About"),
             icon=self.getAssetIcon("help.png")
