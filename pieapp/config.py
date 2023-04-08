4fdeed2bb4917ba37db4a8d4798f7bcea80bfdd4
uselessvevo <jcommander13@yandex.ru>
2023-04-08 19:49:56 +0500

    Move `TOOL_BUTTON_ICON_SIZE` from pieapp/config in workbench's new config file

--- pieapp/config.py
+++ pieapp/config.py
@@ -5,6 +5,3 @@ PIEAPP_VERSION: Lock = "2023.04.pre-alpha"
 
 # List of excluded file formats
 ASSETS_EXCLUDED_FORMATS = [DirectoryType, ".qss", ".json", ".ttf", ".py"]
-
-# QToolButton icon size
-TOOL_BUTTON_ICON_SIZE: Lock = (25, 25)
---
+++ pieapp/containers/workbench/plugin/config.py
@@ -0,0 +1,4 @@
+from piekit.config.types import Lock
+
+# QToolButton icon size
+TOOL_BUTTON_ICON_SIZE: Lock = (25, 25)
