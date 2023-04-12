351f78c99c0f6ff0b8732d7a056f1c6d8702a869
k.ivanov <k.ivanov@tnpz.rusinvest.ru>
2023-04-12 08:00:09 +0500

    Minor fixes in ConfigLoad

--- piekit/config/loader.py
+++ piekit/config/loader.py
@@ -1,6 +1,7 @@
-import types
 import warnings
 import importlib
+from typing import Any
+from types import ModuleType
 
 from piekit.config.types import Lock
 
@@ -16,35 +17,38 @@ class ConfigLoader:
     def import_module(self, import_path: str) -> None:
         """
         Load configuration module
-
         Args:
             import_path (str): configuration module import path
         """
         try:
-            config_module: types.ModuleType = importlib.import_module(import_path)
+            config_module: ModuleType = importlib.import_module(import_path)
         except ModuleNotFoundError as e:
             raise e
 
         self.load_module(config_module)
 
-    def load_module(self, config_module: types.ModuleType) -> None:
-        module_attributes: dict = {
-            k: v for (k, v) in config_module.__dict__.items()
-            if k.isupper()
+    def load_module(self, config_module: ModuleType) -> None:
+        """
+        Process configuration module
+
+        Args:
+            config_module (ModuleType): configuration module
+        """
+        temp_locked_attributes: list[str] = []
+        module_attributes: dict[str, Any] = {
+            k: v for (k, v) in config_module.__dict__.items() if k.isupper()
         }
         module_locked_attributes: dict = {
             k: v for (k, v) in getattr(config_module, "__annotations__", {}).items()
             if k.isupper() and issubclass(v, Lock)
         }
-        temp_locked_attributes = []
 
         for name, value in module_attributes.items():
             if name in module_locked_attributes:
                 if name in self.locked_attributes:
-                    warnings.warn(f"{name} is locked - you can't change the value of it")
+                    warnings.warn(f"{name} is locked - you can't change its value")
                     continue
                 else:
-                    setattr(self, name, value)
                     temp_locked_attributes.append(name)
 
             setattr(self, name, value)
