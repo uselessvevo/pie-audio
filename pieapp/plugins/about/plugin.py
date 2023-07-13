011e1e99534c66748e492dae2ed812d2c72cdf06
k.ivanov <k.ivanov@tnpz.rusinvest.ru>
2023-07-13 08:19:24 +0500

    Minor fixes in About plugin

--- pieapp/plugins/about/plugin.py
+++ pieapp/plugins/about/plugin.py
@@ -26,9 +26,6 @@ class About(
     AssetsAccessor,
 ):
     name = Plugin.About
-    version: str = "1.0.0"
-    pieapp_version: str = "1.0.0"
-    piekit_version: str = "1.0.0"
     requires = [Container.MenuBar]
 
     def call(self) -> None:
