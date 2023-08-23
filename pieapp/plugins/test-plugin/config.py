6fd2e1a3d03e722f155cd9f4381c578e87b9e074
uselessvevo <jcommander13@yandex.ru>
2023-08-23 21:32:45 +0500

    Fixes in test-plugin/config.py

--- pieapp/plugins/test-plugin/config.py
+++ pieapp/plugins/test-plugin/config.py
@@ -2,8 +2,8 @@ import os
 
 from piekit.config import Max, Min, Lock
 
-TEST_PLUGIN_ENABLE: Lock = os.getenv("PIE_TEST_PLUGIN_ENABLE", False)
-REGISTER_ON_MAIN_LAYOUT: Lock = os.getenv("PIE_TEST_PLUGIN_REGISTER_ON_MW_LAYOUT", False)
+TEST_PLUGIN_ENABLE: Lock = bool(int(os.getenv("PIE_TEST_PLUGIN_ENABLE", False)))
+REGISTER_ON_MAIN_LAYOUT: Lock = bool(int(os.getenv("PIE_TEST_PLUGIN_REGISTER_ON_MW_LAYOUT", False)))
 
 FIELD_MIN: Min[3] = [1, 2]
 FIELD_MAX: Max[3] = [1, 2, 3, 4]
