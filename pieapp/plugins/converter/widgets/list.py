81d8c358febd13bdce66686476cb764ee98e6411
KIvanov <KIvanov@rubytech.ru>
2024-08-27 11:56:41 +0300

    Parent argument removed from ConverterListWidget

--- pieapp/plugins/converter/widgets/list.py
+++ pieapp/plugins/converter/widgets/list.py
@@ -1,19 +1,19 @@
 from __feature__ import snake_case
 
 from PySide6.QtGui import Qt
-from PySide6.QtCore import QObject
-from PySide6.QtWidgets import QListWidget, QSizePolicy, QAbstractItemView
+from PySide6.QtWidgets import QListWidget
+from PySide6.QtWidgets import QSizePolicy
+from PySide6.QtWidgets import QAbstractItemView
 
 
 class ConverterListWidget(QListWidget):
 
     def __init__(
         self,
-        parent: QObject = None,
         change_callback: callable = None,
         remove_callback: callable = None
     ) -> None:
-        super().__init__(parent)
+        super().__init__()
         self.set_object_name("ConverterList")
         self.set_contents_margins(0, 0, 0, 0)
 
