015caffcb675b19cee292b2f7cf94b10aa784dc3
KIvanov <KIvanov@rubytech.ru>
2025-01-15 17:39:05 +0300

    Minor update in pieapp/api/plugins - new methdod `get_plugin_widget`

--- pieapp/api/plugins/helpers.py
+++ pieapp/api/plugins/helpers.py
@@ -3,8 +3,12 @@ Plugin utils
 """
 from typing import Any
 
-from pieapp.api.plugins.registry import Plugins
+from pieapp.api.plugins.registry import PluginRegistry
 
 
 def get_plugin(plugin: str) -> Any:
-    return Plugins.get_plugin(plugin)
+    return PluginRegistry.get_plugin(plugin)
+
+
+def get_plugin_widget(plugin: str) -> Any:
+    return PluginRegistry.get_plugin(plugin).get_widget()
