83b595f62ee90b1e00dedb5d1bca551287afca60
KIvanov <KIvanov@rubytech.ru>
2025-01-15 18:05:33 +0300

    Rename PieException -> PieError

--- pieapp/api/exceptions.py
+++ pieapp/api/exceptions.py
@@ -4,23 +4,21 @@ System exceptions
 from pieapp.api.registries.locales.helpers import translate
 
 
-class PieException(Exception):
+class PieError(Exception):
+    error_type: str = "default"
 
     def __init__(
         self,
         title: str = None,
         description: str = None,
-        code: int = None,
         raise_exception: bool = False
     ) -> None:
-        self._code = code
         self._title = translate(title or "Error")
         self._description = translate(description or "An error has been occurred")
         self._raise_exception = raise_exception
 
-    def get_dict(self) -> dict:
+    def as_dict(self) -> dict:
         return {
-            "code": self._code,
             "title": self._title,
             "description": self._description,
             "raise_exception": self._raise_exception
@@ -28,3 +26,7 @@ class PieException(Exception):
 
     def __str__(self) -> str:
         return f"{self._title} - {self._description}"
+
+
+class NotificationError(PieError):
+    error_type = "info"
