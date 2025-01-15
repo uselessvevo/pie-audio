76e98c917b61253f57b94e28a79227ba436d1cc5
KIvanov <KIvanov@rubytech.ru>
2025-01-15 17:41:06 +0300

    Minor update in pieapp/api/registries/themes

--- pieapp/api/registries/themes/helpers.py
+++ pieapp/api/registries/themes/helpers.py
@@ -4,8 +4,8 @@ from typing import Any, Union
 
 from PySide6.QtGui import QPixmap, QPainter, QColor, QIcon
 
-from pieapp.api.registries.models import Scope
-from pieapp.api.registries.themes.manager import Themes
+from pieapp.api.models.scopes import Scope
+from pieapp.api.registries.themes.registry import ThemeRegistry
 
 
 def as_svg(file: str, color: Union[str, list] = None) -> QIcon:
@@ -25,36 +25,36 @@ def as_svg(file: str, color: Union[str, list] = None) -> QIcon:
 def get_file_path(
     key: Any,
     default: Any = None,
-    scope: Union[str, Scope] = Scope.Shared
+    scope: str = Scope.Shared
 ) -> Any:
-    return Themes.get(scope, key, default)
+    return ThemeRegistry.get(scope, key, default)
 
 
 def get_icon(
     key: Any,
     default: Any = None,
-    scope: Union[str, Scope] = Scope.Shared
+    scope: str = Scope.Shared
 ) -> QIcon:
-    return QIcon(Themes.get(scope, key, default))
+    return QIcon(ThemeRegistry.get(scope, key, default))
 
 
 def get_svg_icon(
     key: Any,
     color: str,
     default: Any = None,
-    scope: Union[str, Scope] = Scope.Shared
+    scope: str = Scope.Shared
 ) -> QIcon:
-    icon_path = Themes.get(scope, key, default)
+    icon_path = ThemeRegistry.get(scope, key, default)
     return as_svg(icon_path, color)
 
 
 def get_current_theme() -> str:
-    return Themes.get_theme()
+    return ThemeRegistry.get_theme()
 
 
 def get_themes() -> list[str]:
-    return Themes.get_themes()
+    return ThemeRegistry.get_themes()
 
 
 def get_theme_property(prop_name: str, default: Any = None) -> Any:
-    return Themes.get_theme_property(prop_name, default)
+    return ThemeRegistry.get_theme_property(prop_name, default)
