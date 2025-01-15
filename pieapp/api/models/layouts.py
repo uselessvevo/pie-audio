daa7b8f917cfd8a3e7a55860a36039ef7cc3e71c
KIvanov <KIvanov@rubytech.ru>
2025-01-15 17:44:16 +0300

    New layouts

--- pieapp/api/models/layouts.py
+++ pieapp/api/models/layouts.py
@@ -4,4 +4,18 @@ Built-in layouts
 
 
 class Layout:
+    # Tools layout is used to display tool widgets.
+    # For example: toolbar, additional menus, etc.
+    # Uses `QHBoxLayout`
+    # P.S: Menus from `MainWindow` are not registered on this layout
+    Tools = "tools"
+
+    # Canvas layout is used to display main widgets
+    # For example: converter's list widget
+    # Uses `QGridLayout`
     Main = "main"
+
+    # Info layout is used to display information
+    # For example: status bar, etc.
+    # Uses `QHBoxLayout`
+    Info = "info"
